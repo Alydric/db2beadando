b7812fb80261160492738f5fb05ddb6aa962940e
Alydric <levente2727@gmail.com>
2020-11-26 17:22:44 +0100

    oracle importálás
    Co-Authored-By: alydric <55102134+alydric@users.noreply.github.com>

---
+++ beadando/beadando/AdatbazisKapcsolat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace beadando
+{
+    class AdatbazisKapcsolat
+    {
+        public OracleConnection kapcsolatNyit()
+        {
+            OracleConnection kapcsolat = new OracleConnection();
+            string kapcsolatiString = @"...";
+
+            kapcsolat.ConnectionString = kapcsolatiString;
+            kapcsolat.Open();
+
+            return kapcsolat;
+        }
+    }
+}
