83ec37adafb2e8ae0217e8a780c8df29e5b492c5
Alydric <levente2727@gmail.com>
2020-11-26 19:00:49 +0100

    Update Tulaj.cs
    Co-Authored-By: alydric <55102134+alydric@users.noreply.github.com>

--- beadando/beadando/Tulaj.cs
+++ beadando/beadando/Tulaj.cs
@@ -12,5 +12,45 @@ namespace beadando
         private int id;
         private DateTime dateOfBirth;
         
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("A név nem lehet üres!");
+                if (250 < value.Length)
+                    throw new ArgumentOutOfRangeException("A mező max 250 karakter lehet!");
+                name = value;
+            }
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new Exception("Nem lehet a mai naptól későbbi dátumot beállítani!");
+                dateOfBirth = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
