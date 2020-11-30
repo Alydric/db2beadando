0a0eab711becbf85e3d8330406c6c13b4fc9a371
Alydric <levente2727@gmail.com>
2020-11-30 11:56:43 +0100

    tulajkezelo delete + insert
    Co-Authored-By: Alydric <55102134+Alydric@users.noreply.github.com>

--- beadando/beadando/TulajKezelo.cs
+++ beadando/beadando/TulajKezelo.cs
@@ -31,5 +31,54 @@ namespace beadando
             reader.Close();
             return tulajok;
         }
+
+        public int Delete(Tulaj torlendo)
+        {
+            OracleCommand parancs = new OracleCommand();
+            parancs.Connection = kapcsolatNyit();
+            parancs.CommandType = System.Data.CommandType.Text;
+            parancs.CommandText = "DELETE FROM tulajok WHERE id = :id";
+
+            OracleParameter pId = new OracleParameter();
+            pId.ParameterName = ":id";
+            pId.OracleDbType = OracleDbType.Int32;
+            pId.Direction = System.Data.ParameterDirection.Input;
+            pId.Value = torlendo.Id;
+            parancs.Parameters.Add(pId);
+
+            return parancs.ExecuteNonQuery();
+        }
+
+        public int Insert(Tulaj uj)
+        {
+            OracleCommand p = new OracleCommand();
+            p.Connection = kapcsolatNyit();
+            p.CommandType = System.Data.CommandType.Text;
+            p.CommandText = "INSERT INTO tulajok(id, name, dateOfBirth) VALUES(:id, :name, :dateOfBirth)";
+
+            OracleParameter pId = new OracleParameter();
+            pId.ParameterName = ":id";
+            pId.OracleDbType = OracleDbType.Int32;
+            pId.Direction = System.Data.ParameterDirection.Input;
+            pId.Value = uj.Id;
+
+            OracleParameter pName = new OracleParameter();
+            pName.ParameterName = ":name";
+            pName.OracleDbType = OracleDbType.Varchar2;
+            pName.Direction = System.Data.ParameterDirection.Input;
+            pName.Value = uj.Name;
+
+            OracleParameter pDateOfBirth = new OracleParameter();
+            pDateOfBirth.ParameterName = ":dateOfBirth";
+            pDateOfBirth.OracleDbType = OracleDbType.Date;
+            pDateOfBirth.Direction = System.Data.ParameterDirection.Input;
+            pDateOfBirth.Value = uj.DateOfBirth;
+
+            p.Parameters.Add(pId);
+            p.Parameters.Add(pName);
+            p.Parameters.Add(pDateOfBirth);
+
+            return p.ExecuteNonQuery();
+        }
     }
 }
