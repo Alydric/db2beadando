0e991c39d4ddb42dd418845224d0b86175cb6bd4
Alydric <levente2727@gmail.com>
2020-12-12 14:20:38 +0100

    folytatás
    tulaj tab, törlés gomb click event + metódusok
    
    Co-Authored-By: Alydric <55102134+Alydric@users.noreply.github.com>

--- beadando/beadando/Form1.Designer.cs
+++ beadando/beadando/Form1.Designer.cs
@@ -39,23 +39,35 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.BtInsert = new System.Windows.Forms.Button();
+            this.BtCarInsert = new System.Windows.Forms.Button();
             this.TbRendszam = new System.Windows.Forms.TextBox();
             this.TbMarka = new System.Windows.Forms.TextBox();
             this.TbTipus = new System.Windows.Forms.TextBox();
             this.TbEvjarat = new System.Windows.Forms.TextBox();
             this.TbHp = new System.Windows.Forms.TextBox();
             this.TbCcm = new System.Windows.Forms.TextBox();
-            this.TbId = new System.Windows.Forms.TextBox();
+            this.TbCarId = new System.Windows.Forms.TextBox();
             this.CbTulajdonos = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.BtTorles = new System.Windows.Forms.Button();
+            this.BtCarTorles = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.CbCars = new System.Windows.Forms.ComboBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.BtTulajTorles = new System.Windows.Forms.Button();
+            this.CbTulajok = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.TbName = new System.Windows.Forms.TextBox();
+            this.TbDateOfBirth = new System.Windows.Forms.TextBox();
+            this.TbTulajId = new System.Windows.Forms.TextBox();
+            this.BtTulajInsert = new System.Windows.Forms.Button();
             this.tabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // oracleCommand1
@@ -83,17 +95,17 @@
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Controls.Add(this.BtInsert);
+            this.tabPage1.Controls.Add(this.BtCarInsert);
             this.tabPage1.Controls.Add(this.TbRendszam);
             this.tabPage1.Controls.Add(this.TbMarka);
             this.tabPage1.Controls.Add(this.TbTipus);
             this.tabPage1.Controls.Add(this.TbEvjarat);
             this.tabPage1.Controls.Add(this.TbHp);
             this.tabPage1.Controls.Add(this.TbCcm);
-            this.tabPage1.Controls.Add(this.TbId);
+            this.tabPage1.Controls.Add(this.TbCarId);
             this.tabPage1.Controls.Add(this.CbTulajdonos);
             this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.BtTorles);
+            this.tabPage1.Controls.Add(this.BtCarTorles);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.CbCars);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
@@ -176,14 +188,15 @@
             this.label3.TabIndex = 13;
             this.label3.Text = "Tulajdonos";
             // 
-            // BtInsert
+            // BtCarInsert
             // 
-            this.BtInsert.Location = new System.Drawing.Point(431, 430);
-            this.BtInsert.Name = "BtInsert";
-            this.BtInsert.Size = new System.Drawing.Size(243, 52);
-            this.BtInsert.TabIndex = 12;
-            this.BtInsert.Text = "Insert";
-            this.BtInsert.UseVisualStyleBackColor = true;
+            this.BtCarInsert.Location = new System.Drawing.Point(431, 430);
+            this.BtCarInsert.Name = "BtCarInsert";
+            this.BtCarInsert.Size = new System.Drawing.Size(243, 52);
+            this.BtCarInsert.TabIndex = 12;
+            this.BtCarInsert.Text = "Insert";
+            this.BtCarInsert.UseVisualStyleBackColor = true;
+            this.BtCarInsert.Click += new System.EventHandler(this.BtCarInsert_Click);
             // 
             // TbRendszam
             // 
@@ -227,12 +240,12 @@
             this.TbCcm.Size = new System.Drawing.Size(294, 22);
             this.TbCcm.TabIndex = 6;
             // 
-            // TbId
+            // TbCarId
             // 
-            this.TbId.Location = new System.Drawing.Point(402, 122);
-            this.TbId.Name = "TbId";
-            this.TbId.Size = new System.Drawing.Size(294, 22);
-            this.TbId.TabIndex = 5;
+            this.TbCarId.Location = new System.Drawing.Point(402, 122);
+            this.TbCarId.Name = "TbCarId";
+            this.TbCarId.Size = new System.Drawing.Size(294, 22);
+            this.TbCarId.TabIndex = 5;
             // 
             // CbTulajdonos
             // 
@@ -252,14 +265,15 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Insert";
             // 
-            // BtTorles
+            // BtCarTorles
             // 
-            this.BtTorles.Location = new System.Drawing.Point(14, 135);
-            this.BtTorles.Name = "BtTorles";
-            this.BtTorles.Size = new System.Drawing.Size(171, 85);
-            this.BtTorles.TabIndex = 2;
-            this.BtTorles.Text = "Törlés";
-            this.BtTorles.UseVisualStyleBackColor = true;
+            this.BtCarTorles.Location = new System.Drawing.Point(14, 135);
+            this.BtCarTorles.Name = "BtCarTorles";
+            this.BtCarTorles.Size = new System.Drawing.Size(171, 85);
+            this.BtCarTorles.TabIndex = 2;
+            this.BtCarTorles.Text = "Törlés";
+            this.BtCarTorles.UseVisualStyleBackColor = true;
+            this.BtCarTorles.Click += new System.EventHandler(this.BtCarTorles_Click);
             // 
             // label1
             // 
@@ -281,6 +295,17 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.BtTulajInsert);
+            this.tabPage2.Controls.Add(this.label13);
+            this.tabPage2.Controls.Add(this.label14);
+            this.tabPage2.Controls.Add(this.label15);
+            this.tabPage2.Controls.Add(this.TbName);
+            this.tabPage2.Controls.Add(this.TbDateOfBirth);
+            this.tabPage2.Controls.Add(this.TbTulajId);
+            this.tabPage2.Controls.Add(this.BtTulajTorles);
+            this.tabPage2.Controls.Add(this.CbTulajok);
+            this.tabPage2.Controls.Add(this.label12);
+            this.tabPage2.Controls.Add(this.label11);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -288,6 +313,102 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Tulajdonosok";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(31, 41);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(173, 36);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Tulajdonos";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(372, 41);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(95, 36);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Insert";
+            // 
+            // BtTulajTorles
+            // 
+            this.BtTulajTorles.Location = new System.Drawing.Point(37, 151);
+            this.BtTulajTorles.Name = "BtTulajTorles";
+            this.BtTulajTorles.Size = new System.Drawing.Size(171, 85);
+            this.BtTulajTorles.TabIndex = 6;
+            this.BtTulajTorles.Text = "Törlés";
+            this.BtTulajTorles.UseVisualStyleBackColor = true;
+            this.BtTulajTorles.Click += new System.EventHandler(this.BtTulajTorles_Click);
+            // 
+            // CbTulajok
+            // 
+            this.CbTulajok.FormattingEnabled = true;
+            this.CbTulajok.Location = new System.Drawing.Point(37, 97);
+            this.CbTulajok.Name = "CbTulajok";
+            this.CbTulajok.Size = new System.Drawing.Size(203, 24);
+            this.CbTulajok.TabIndex = 5;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(262, 219);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(110, 17);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Születési Dátum";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(339, 151);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(33, 17);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "Név";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(351, 102);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(21, 17);
+            this.label15.TabIndex = 20;
+            this.label15.Text = "ID";
+            // 
+            // TbName
+            // 
+            this.TbName.Location = new System.Drawing.Point(378, 151);
+            this.TbName.Name = "TbName";
+            this.TbName.Size = new System.Drawing.Size(294, 22);
+            this.TbName.TabIndex = 19;
+            // 
+            // TbDateOfBirth
+            // 
+            this.TbDateOfBirth.Location = new System.Drawing.Point(378, 214);
+            this.TbDateOfBirth.Name = "TbDateOfBirth";
+            this.TbDateOfBirth.Size = new System.Drawing.Size(294, 22);
+            this.TbDateOfBirth.TabIndex = 18;
+            // 
+            // TbTulajId
+            // 
+            this.TbTulajId.Location = new System.Drawing.Point(378, 99);
+            this.TbTulajId.Name = "TbTulajId";
+            this.TbTulajId.Size = new System.Drawing.Size(294, 22);
+            this.TbTulajId.TabIndex = 17;
+            // 
+            // BtTulajInsert
+            // 
+            this.BtTulajInsert.Location = new System.Drawing.Point(396, 285);
+            this.BtTulajInsert.Name = "BtTulajInsert";
+            this.BtTulajInsert.Size = new System.Drawing.Size(243, 52);
+            this.BtTulajInsert.TabIndex = 23;
+            this.BtTulajInsert.Text = "Insert";
+            this.BtTulajInsert.UseVisualStyleBackColor = true;
+            this.BtTulajInsert.Click += new System.EventHandler(this.BtTulajInsert_Click);
             // 
             // Form1
             // 
@@ -300,6 +421,8 @@
             this.tabControl.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -310,7 +433,7 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.ComboBox CbTulajdonos;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button BtTorles;
+        private System.Windows.Forms.Button BtCarTorles;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox CbCars;
         private System.Windows.Forms.TabPage tabPage2;
@@ -321,7 +444,7 @@
         private System.Windows.Forms.TextBox TbEvjarat;
         private System.Windows.Forms.TextBox TbHp;
         private System.Windows.Forms.TextBox TbCcm;
-        private System.Windows.Forms.TextBox TbId;
+        private System.Windows.Forms.TextBox TbCarId;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
@@ -330,7 +453,18 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button BtInsert;
+        private System.Windows.Forms.Button BtCarInsert;
+        private System.Windows.Forms.Button BtTulajTorles;
+        private System.Windows.Forms.ComboBox CbTulajok;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button BtTulajInsert;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox TbName;
+        private System.Windows.Forms.TextBox TbDateOfBirth;
+        private System.Windows.Forms.TextBox TbTulajId;
     }
 }
 
--- beadando/beadando/Form1.cs
+++ beadando/beadando/Form1.cs
@@ -30,9 +30,75 @@ namespace beadando
             tulajs = tulajKezelo.Select();
             //feltoltComboBoxTulajok();
         }
-
         
+        private void FeltoltCbCars()
+        {
+            CbCars.BeginUpdate();
+            CbCars.Items.Clear();
 
-        
+            foreach (Car car in cars)
+            {
+                CbCars.Items.Add(car);
+            }
+            CbCars.EndUpdate();
+        }
+
+        private void FeltoltCbTulajok()
+        {
+            CbTulajok.BeginUpdate();
+            CbTulajok.Items.Clear();
+
+            foreach (Tulaj tulaj in tulajs)
+            {
+                CbTulajok.Items.Add(tulaj);
+            }
+            CbTulajok.EndUpdate();
+        }
+
+        private void BtCarTorles_Click(object sender, EventArgs e)
+        {
+            if (CbCars.SelectedIndex == -1)
+            {
+                MessageBox.Show("Először válassz ki egy elemet!");
+                return;
+            }
+
+            Car torlendo = (Car)CbCars.SelectedItem;
+
+            int toroltRekordokDb = carKezelo.Delete(torlendo);
+            MessageBox.Show(toroltRekordokDb + "rekord sikeresen törölve lett!");
+            CbCars.SelectedItem = null;
+            cars = carKezelo.Select();
+
+            FeltoltCbCars();
+        }
+
+        private void BtTulajTorles_Click(object sender, EventArgs e)
+        {
+            if (CbTulajok.SelectedIndex == -1)
+            {
+                MessageBox.Show("Először válassz ki egy elemet!");
+                return;
+            }
+
+            Tulaj torlendo = (Tulaj)CbTulajok.SelectedItem;
+
+            int toroltRekordokDb = tulajKezelo.Delete(torlendo);
+            MessageBox.Show(toroltRekordokDb + "rekord sikeresen törölve lett!");
+            CbTulajok.SelectedItem = null;
+            tulajs = tulajKezelo.Select();
+
+            FeltoltCbTulajok();
+        }
+
+        private void BtCarInsert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtTulajInsert_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
